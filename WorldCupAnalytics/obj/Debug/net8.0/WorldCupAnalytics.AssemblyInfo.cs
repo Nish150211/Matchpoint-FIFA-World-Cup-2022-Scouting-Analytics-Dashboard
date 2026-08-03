83e0b4b3377bfb40f30fa0a05ddc86437682a3d6
Nish150211 <nathoonishaal@gmail.com>
2026-08-03 17:08:16 +0200

    j

--- WorldCupAnalytics/obj/Debug/net8.0/WorldCupAnalytics.AssemblyInfo.cs
+++ WorldCupAnalytics/obj/Debug/net8.0/WorldCupAnalytics.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldCupAnalytics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9674ce1a2f24ec3da2227cc8a2713a1bba1b3cf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldCupAnalytics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldCupAnalytics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
